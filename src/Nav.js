200b8931f9bce63e2afac67bac91623e8b38c46e
kyhyeok <kyhyeok@gmail.com>
2020-09-18 17:31:43 +0900

    netflix-clone

---
+++ src/Nav.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+        className="nav__logo"
+      />
+      <img src="" alt="Netflix Logo" className="nav__avatar" />
+    </div>
+  );
+}
+
+export default Nav;
